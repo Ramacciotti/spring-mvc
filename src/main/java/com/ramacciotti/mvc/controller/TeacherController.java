f94318eb79bbeafa66d06ae1f88a241b4c489dad
Mariana Ramacciotti <marrie.ciotti@gmail.com>
2023-02-18 00:08:16 -0300

    removing unused code

--- src/main/java/com/ramacciotti/mvc/controller/TeacherController.java
+++ src/main/java/com/ramacciotti/mvc/controller/TeacherController.java
@@ -1,9 +1,8 @@
 package com.ramacciotti.mvc.controller;
 
 import com.ramacciotti.mvc.dto.TeacherRequestDTO;
-import com.ramacciotti.mvc.model.Teacher;
 import com.ramacciotti.mvc.model.Status;
-import com.ramacciotti.mvc.repository.TeacherRepository;
+import com.ramacciotti.mvc.model.Teacher;
 import com.ramacciotti.mvc.service.TeacherService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,9 +22,6 @@ public class TeacherController {
     @Autowired
     TeacherService teacherService;
 
-    @Autowired
-    TeacherRepository teacherRepository;
-
     @GetMapping("/form/show")
     public ModelAndView showForm() {
         log.info(">> showForm()");
@@ -36,27 +32,18 @@ public class TeacherController {
 
     @PostMapping("/form/submit")
     public String submitForm(TeacherRequestDTO teacherRequestDTO) {
-        System.out.println(teacherRequestDTO.toString());
-        return "redirect:/teacher/read";
+        log.info(">> submitForm()");
+        teacherService.create(teacherRequestDTO);
+        return "redirect:/teacher/list";
     }
 
-    @GetMapping("/read")
+    @GetMapping("/list")
     public ModelAndView read() {
-        log.info(">> read()");
+        log.info(">> list()");
         List<Teacher> teachers = teacherService.read();
         ModelAndView modelAndView = new ModelAndView("read_list.html");
         modelAndView.addObject("teachers", teachers);
         return modelAndView;
     }
-//
-//    @PostMapping("/form/submit")
-//    public ModelAndView submitForm(TeacherRequestDTO teacherRequestDTO) {
-//        log.info(">> submitForm()");
-//        TeacherResponseDTO teacherResponseDTO = teacherService.create(teacherRequestDTO);
-//        ModelAndView modelAndView = new ModelAndView("post_success.html");
-//        modelAndView.addObject("name", teacherResponseDTO.getName());
-//        return modelAndView;
-//    }
-
 
 }
